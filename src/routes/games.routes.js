3ca0ec03aaa48275ce38b81fd605749664ad0dc8
joseedivandro <jose.edivandro.ja@outlook.com>
2023-05-12 16:26:39 -0300

    corrigindo falhas na roda post games

--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { getGames, createGames } from "../controllers/games.controller.js";
-import { validateSchema } from "../middlewares/validateSchema.middleware.js";
+
 
 const gamesRouter = Router()
 
 gamesRouter.get('/games', getGames)
 
-gamesRouter.post('post', createGames)
+gamesRouter.post('/games', createGames)
 
 
 export default gamesRouter

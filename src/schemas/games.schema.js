d7ad78197a06db3437a3e77c28b721ed7036fcbf
joseedivandro <jose.edivandro.ja@outlook.com>
2023-05-12 17:47:38 -0300

    ajeitando schema dos games novamente

--- src/schemas/games.schema.js
+++ src/schemas/games.schema.js
@@ -3,7 +3,7 @@ import joi from "joi"
 export const GamesRules = joi.object({
 
     name: joi.string().empty().required(),
-    image: Joi.string().trim().uri().required(),
+    image: joi.string().trim().uri().required(),
     stockTotal: joi.number().integer().min(1).required(),
     pricePerDay: joi.number().integer().min(1).required(),
    
